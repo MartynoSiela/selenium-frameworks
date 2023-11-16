63b391559edc193a32a88768758a0a636d9104fc
MartynoSiela <m.satas@gmail.com>
2023-11-16 15:50:36 +0200

    Add demo code of static page object model with common methods class

--- Framework/Pages/Brave/SearchPage.cs
+++ Framework/Pages/Brave/SearchPage.cs
@@ -1,27 +1,27 @@
-﻿using OpenQA.Selenium;
-
-namespace Framework.Pages.Brave
+﻿namespace Framework.Pages.Brave
 {
     public class SearchPage
     {
         public static void Open()
         {
-            Driver.GetDriver().Url = "https://search.brave.com/";
+            Common.OpenPage("https://search.brave.com/");
         }
 
         public static void EnterSearchPhrase(string phrase)
         {
-            Driver.GetDriver().FindElement(By.Id("searchbox")).SendKeys(phrase);
+            string locator = "//*[@id='searchbox']";
+            Common.SendKeysToElement(locator, phrase);
         }
 
         public static void ClickButtonSubmit()
         {
-            Driver.GetDriver().FindElement(By.Id("submit-button")).Click();
+            string locator = "//*[@id='submit-button']";
+            Common.ClickElement(locator);
         }
 
         public static string GetPageTitle()
         {
-            return Driver.GetDriver().Title;
+            return Common.GetPageTitle();
         }
     }
 }
---
+++ Framework/Pages/Common.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+
+namespace Framework.Pages
+{
+    internal class Common
+    {
+        internal static void OpenPage(string url)
+        {
+            Driver.GetDriver().Url = url;
+        }
+
+        internal static string GetPageTitle()
+        {
+            return Driver.GetDriver().Title;
+        }
+
+        private static IWebElement GetElementByXpath(string xpath)
+        {
+            return Driver.GetDriver().FindElement(By.XPath(xpath));
+        }
+
+        internal static void SendKeysToElement(string locatorXpath, string keys)
+        {
+            GetElementByXpath(locatorXpath).SendKeys(keys);
+        }
+
+        internal static void ClickElement(string locatorXpath)
+        {
+            GetElementByXpath(locatorXpath).Click();
+        }
+    }
+}
